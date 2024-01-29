ddc3a391fae94749d12ef63e693110ffc2e9308a
Prashant-flick <prashantkumar1251@gmail.com>
2024-01-29 15:37:54 +0530

    test

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import appwriteService from '../appwrite/config'
-import {Link} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function PostCard({
   $id,
   featuredImage,
   title
 }) {
+  if(!featuredImage){
+    const [post, setPost] = React.useState(null)
+    const navigate = useNavigate()
+    const posts = useSelector((state) => state.dataReducer.data)
+    const slug = useParams()
+    if(slug){
+      if(posts && posts.length > 0){
+        posts.map((post) => {
+          if(post.title === slug){
+            setPost(post)
+          }
+        })
+      }else{
+        appwriteService.getPost(slug)
+        .then((post) => {
+          if(post){
+            setPost(post)
+          }else{
+            navigate("/")
+          }
+        })
+      }
+      featuredImage=post.featuredImage;
+      if(!featuredImage){
+        navigate("/")
+      }
+    }
+  }
+  
+
   return (
     <Link to={`/post/${$id}`}>
       <div className='w-full bg-gray-100 rounded-xl p-4'>
